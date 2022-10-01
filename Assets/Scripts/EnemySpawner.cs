3fd1ec886e7ca302bf5688cc8b87a84c80df6cdb
Lorenzo <illorenzobianchi@gmail.com>
2022-10-01 18:46:25 +0200

    add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private GameObject spawnedEnemy;
+    void Start()
+    {
+        StartCoroutine(SpawnMonsters());
+    }
+    IEnumerator SpawnMonsters()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            spawnedEnemy = PoolingManager.Instantiate(PoolingManager.Type.Enemy, PrefabsManager.GetPrefab(PrefabsManager.PrefabType.Enemy), transform.position, Quaternion.identity);
+            spawnedEnemy.GetComponent<Enemy>().speed = 5;
+
+        }
+    }
+}
